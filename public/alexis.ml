9839c7fb6734ce337e1c1fa9b7df67ad952ea336
glee <g.lee13770334@gmail.com>
2025-12-26 20:15:47 -0500

    fix -- expose util modules from internal lib

--- public/alexis.ml
+++ public/alexis.ml
@@ -1,3 +1,5 @@
 module Lexer = Alexis_lib.Lexer
 module Lexer_intf = Alexis_lib.Lexer_intf
 module Regex_dfa = Regex_dfa
+module Source_position = Alexis_lib.Source_position
+module With_errors = Alexis_lib.With_errors
